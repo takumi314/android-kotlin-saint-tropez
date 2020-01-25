c38a7fc436d20f78de3947604bec9bb1acf1e993
takumi314 <isospin_symmetry@me.com>
2020-01-26 02:11:26 +0900

    show internal HTML file

--- app/src/main/java/com/example/sainttropez/MainActivity.kt
+++ app/src/main/java/com/example/sainttropez/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.sainttropez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // HTMLを表示する
+        showHTML()
+    }
+
+    private fun showHTML() {
+        // JavaScriptを有効にする
+        webView.settings.javaScriptEnabled = true
+        // アプリ内のaasetsフォルダ先は「file//:android_asset/」でアクセスする
+        webView.loadUrl("file//:android_asset/html/index.html")
     }
 }
