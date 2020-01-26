27870492a696c809e8b4d9f6c29ea4ebe636d63c
takumi314 <isospin_symmetry@me.com>
2020-01-26 23:24:18 +0900

    implement OptionMenu

--- app/src/main/java/com/example/sainttropez/MainActivity.kt
+++ app/src/main/java/com/example/sainttropez/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.sainttropez
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         // HTMLを表示する
         showHTML()
+    }
+
+    // オプションメニューを初期化する
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // メニューXMLの内容をメニューに設定する
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item?.itemId) {
+            R.id.top -> {
+                webView.loadUrl("file:///android_asset/html/index.html")
+                return true
+            }
+            R.id.lunch01 -> {
+                webView.loadUrl("file:///android_asset/html/lunch01.html")
+                return true
+            }
+            R.id.lunch02 -> {
+                webView.loadUrl("file:///android_asset/html/lunch02.html")
+                return true
+            }
+            R.id.dinner01 -> {
+                webView.loadUrl("file:///android_asset/html/dinner01.html")
+                return true
+            }
+            R.id.dinner02 -> {
+                webView.loadUrl("file:///android_asset/html/dinner02.html")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun showHTML() {
