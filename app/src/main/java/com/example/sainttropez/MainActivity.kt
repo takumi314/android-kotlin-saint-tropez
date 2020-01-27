b0746e38a50989d6efcbe2df390a431a83ae8f41
takumi314 <isospin_symmetry@me.com>
2020-01-27 21:15:12 +0900

    [WIP] implement implicit intent

--- app/src/main/java/com/example/sainttropez/MainActivity.kt
+++ app/src/main/java/com/example/sainttropez/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.sainttropez
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ContextMenu
@@ -41,10 +44,22 @@ class MainActivity : AppCompatActivity() {
     // コンテキストメニューが選択された時にコールされる
     override fun onContextItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            R.id.sms -> return true
-            R.id.mail -> return true
-            R.id.share -> return true
-            R.id.browse -> return true
+            R.id.sms -> {
+                openSMSApp()
+                return true
+            }
+            R.id.mail -> {
+                openMailer()
+                return true
+            }
+            R.id.share -> {
+                openShareApp()
+                return true
+            }
+            R.id.browse -> {
+                openBrowser()
+                return true
+            }
         }
         return super.onContextItemSelected(item)
     }
@@ -82,4 +97,47 @@ class MainActivity : AppCompatActivity() {
         // アプリ内のaasetsフォルダ先は「file//:android_asset/」でアクセスする
         webView.loadUrl("file:///android_asset/html/index.html")
     }
+
+    private fun openSMSApp() {
+        val number = "111-1111-1111"
+        val uri = Uri.parse("sms:$number")
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = uri
+        startActivity(intent)
+    }
+
+    private fun openMailer() {
+        val address = "XXX@YYY.ZZZ"
+        val uri = Uri.parse("mailto:$address")
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.type = "text/plain"
+        intent.data = uri
+        intent.putExtra(Intent.EXTRA_SUBJECT, "タイトル")
+        intent.putExtra(Intent.EXTRA_TEXT, "本文です。")
+        try {
+            startActivity(intent)
+        } catch (e: Exception) {
+            print(e)
+        }
+    }
+
+    private fun openShareApp() {
+        val message = Uri.encode("ここに #コメント")
+        val intent = Intent(Intent.ACTION_VIEW)
+        val uri = Uri.parse("twitter://post?massage=$message")
+        intent.data = uri
+        startActivityForResult(intent, 1)
+    }
+
+    private fun openBrowser() {
+        val uri = Uri.parse("")
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = uri
+        try {
+            startActivity(intent)
+        } catch (e: Exception) {
+            print(e)
+        }
+    }
+
 }
